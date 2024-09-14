6aae102d1f4a42741ac724ba888ecd2ce6924dd1
Hans ZHANG <hans01091@outlook.com>
2024-09-14 17:32:37 +0800

    day2

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,7 +2,14 @@
 pragma solidity ^0.8.26;
 
 contract SimpleStorage{
-    uint256 public thisNum ;
+    uint256 thisNum ;
+
+    struct People {
+        uint256 age;
+        string name;
+    }
+
+    People[] public people;
 
     function store(uint256 _thisNum) public {
         thisNum = _thisNum;
@@ -16,4 +23,10 @@ contract SimpleStorage{
     function add() public pure returns(uint256){
         return (1+1);
     }   
+
+    function addPerson(string memory _name,uint256 _age) public {
+        People memory newPerson = People({age: _age, name: _name});
+        people.push(newPerson);
+    }
+
 } 
